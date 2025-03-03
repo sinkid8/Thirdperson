551d9e10ad4f47ac6e346c17b8cd725d50a2ded1
Sina <sinaha8@yahoo.ca>
2025-03-03 00:19:31 -0800

    Update PlayerController.cs
    Fixed Score

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI; 
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public int score = 0; 
-    public Text scoreText; 
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
 
     void Start()
     {
