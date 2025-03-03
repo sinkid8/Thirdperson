001f55e42e9f195863a245da2bc8a737c2b8218c
Sina <sinaha8@yahoo.ca>
2025-03-02 23:56:33 -0800

    Reupload
    Laptop Crashed and every file not in the assets were gone but i managed to find the history

---
+++ Assets/CoinRotate.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+    public float rotationSpeed = 100f; 
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class PlayerController : MonoBehaviour
 {
     public int score = 0; 
+    public Text scoreText; 
+
+    void Start()
+    {
+        UpdateScoreUI(); 
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             score += 1; 
-            Debug.Log("Coin Collected! Score: " + score);
+            UpdateScoreUI(); 
             Destroy(other.gameObject); 
+        }
+    }
+
+    void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
         }
     }
 }
